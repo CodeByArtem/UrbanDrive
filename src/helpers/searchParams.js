a6b1b8fcb06639bf12bbe4c555ba9960c7f929e9
ArtemGoiT <150847319+ArtemGoiT@users.noreply.github.com>
2024-10-10 20:07:05 +0200

    svg

--- src/helpers/searchParams.js
+++ src/helpers/searchParams.js
@@ -1,35 +1,33 @@
 const searchParamsName = [
-    { name: "ac", searchKey: "isAirConditioner" },
-    { name: "kitchen", searchKey: "isKitchen" },
-    { name: "tv", searchKey: "isTV" },
-    { name: "shower", searchKey: "isShowerOrWC" },
-  ];
-  export const getSearchParams = (searchParams) => {
-    const currentSearchParams = searchParamsName.reduce(
-      (acc, { name, searchKey }) => {
-        const value = searchParams.get(name);
-        if (value) {
-          acc[searchKey] = !!value;
-          return acc;
-        }
-        return acc;
-      },
-      {}
-    );
-  
-    const location = searchParams.get("location");
-    const transmission = searchParams.get("automatic");
-    const form = searchParams.get("vehicleTypes");
-  
-    if (location && location.trim()) {
-      currentSearchParams.location = location.trim();
+  { name: "ac", searchKey: "isAirConditioner" },
+  { name: "kitchen", searchKey: "isKitchen" },
+  { name: "tv", searchKey: "isTV" },
+  { name: "shower", searchKey: "isShowerOrWC" },
+];
+
+export const getSearchParams = (searchParams) => {
+  const currentSearchParams = {};
+
+  searchParamsName.forEach(({ name, searchKey }) => {
+    const value = searchParams.get(name);
+    if (value) {
+      currentSearchParams[searchKey] = true; // Устанавливаем как true, если параметр присутствует
     }
-    if (transmission) {
-      currentSearchParams.transmission = "automatic";
-    }
-    if (form) {
-      currentSearchParams.form = form;
-    }
-  
-    return currentSearchParams;
-  };
+  });
+
+  const location = searchParams.get("location");
+  const transmission = searchParams.get("automatic");
+  const form = searchParams.get("vehicleTypes");
+
+  if (location && location.trim()) {
+    currentSearchParams.location = location.trim();
+  }
+  if (transmission === "on") {
+    currentSearchParams.transmission = "automatic";
+  }
+  if (form) {
+    currentSearchParams.form = form;
+  }
+
+  return currentSearchParams;
+};
